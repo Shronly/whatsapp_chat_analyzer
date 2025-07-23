1fd9edf2dd342af0ec5cb953ed2009a43e980ffb
Shronly <146992703+Shronly@users.noreply.github.com>
2025-07-23 21:57:06 +0530

    Add files via upload

---
+++ preprocessor.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import re
+
+def preprocessor(data):
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'  ##This tells Python to treat the backslashes literally, which is exactly what you want when writing regular expressions.
+
+    messages = re.split(pattern, data)[1:]
+
+    dates = re.findall(pattern, data)
+
+    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+
+    # convert message_date type
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+
+    # separate user and messages
+    users = []
+    messages = []
+    for message in df['user_message']:
+        entry = re.split(r'([\w\W]+?):\s', message)
+        if entry[1:]:  # user name
+            users.append(entry[1])
+            messages.append(entry[2])
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
+    df['users'] = users
+    df['messages'] = messages
+    df.drop(columns=['user_message'], inplace=True)
+
+    df['only_date'] = df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+
+    period = []
+    for hour in df[['day_name', 'hour']]['hour']:
+        if period == 23:
+            period.append(str(hour) + "-" + str('00'))
+        elif period == 0:
+            period.append(str('00') + "-" + str(hour + 1))
+        else:
+            period.append(str(hour) + "-" + str(hour + 1))
+
+    df['period'] = period
+
+
+    return df
